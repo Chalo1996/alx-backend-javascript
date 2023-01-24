67fb1b0b82b5f83fde155de5f8bf737d1768bab5
Chalo1996 <emusyoka759@gmail.com>
2023-01-24 21:00:56 +0300

    thenables

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((res = 'Got a response from the API') => {
-      console.log(res);
-      return { status: 200, body: 'Success' };
-    })
+    .then((obj = { status: 200, body: 'Success' }) => obj)
     .catch((err = new Error()) => err);
+
+  console.log('Got a response from the API');
 }

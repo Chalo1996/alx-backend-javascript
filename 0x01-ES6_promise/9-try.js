1d8e10c6b71cc6fdc4665a4085dab66a3a568b71
Chalo1996 <emusyoka759@gmail.com>
2023-01-28 01:14:29 +0300

    try...catch...finally

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (e) {
-    queue.push(e);
+    queue.push(e.message);
   } finally {
     queue.push('Guardrail was processed');
   }

09140cbbf984fabb910331fffb68e34e742b1fd9
Chalo1996 <emusyoka759@gmail.com>
2023-01-27 15:38:34 +0300

    catching errors in a chain

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then((obj = { status: 200, body: 'success' }) => obj)
-    .catch(new Error())
+    .catch((err = new Error()) => err)
     .finally(console.log('Got a response from the API'));
 }

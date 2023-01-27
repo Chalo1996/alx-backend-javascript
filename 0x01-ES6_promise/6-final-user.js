8fdeb51778d6e65b6585dfcb6b3c741f683c0f85
Chalo1996 <emusyoka759@gmail.com>
2023-01-27 21:08:28 +0300

    Until all promises have been resolved

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([user, photo]) => ([user, photo]))
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then(([user, photo]) => [user, photo])
     .catch((err) => err);
 }

e239380472d0213dfe9159afc6a90ca7b30ef75e
Chalo1996 <emusyoka759@gmail.com>
2023-01-27 17:08:28 +0300

    Rosolve all promises using Promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(photo.body, user.firstName, user.lastName);
-    })
-    .catch((err = 'Signup system offline') => {
-      console.log(err);
-    });
+  return Promise.all([uploadPhoto(), createUser()]);
 }
+
+handleProfileSignup()
+  .then(([photo, user]) => {
+    console.log(photo.body, user.firstName, user.lastName);
+  })
+  .catch((err = 'Signup system offline') => console.log(err));

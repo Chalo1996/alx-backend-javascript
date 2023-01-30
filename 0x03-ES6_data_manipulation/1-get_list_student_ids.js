334bd9732df81de9c065fa0e5c9937dc1b354903
Chalo1996 <emusyoka759@gmail.com>
2023-01-30 18:12:06 +0300

    Using map to filter out data

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,10 +2,6 @@ export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
   }
-  const ids = [];
-  for (let i = 0, len = array.length; i < len; i += 1) {
-    ids.push(array[i].id);
-  }
 
-  return ids;
+  return array.map((ids) => ids.id);
 }

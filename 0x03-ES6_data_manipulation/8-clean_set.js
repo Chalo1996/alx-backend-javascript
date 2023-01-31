a37d983a7ebe819c76e2a1e7ab5dccb0f946c393
Chalo1996 <emusyoka759@gmail.com>
2023-01-31 16:18:10 +0300

    string concatanation and slice

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(set, startString) {
   let newStr = '';
 
+  if (startString === '') {
+    return '';
+  }
+
   for (const word of set) {
-    if (startString.length > 0) {
-      if (word.startsWith(startString)) {
-        newStr += `${word.slice(startString.length)}-`;
-      }
+    if (word.startsWith(startString)) {
+      newStr += `${word.slice(startString.length)}-`;
     }
   }
 

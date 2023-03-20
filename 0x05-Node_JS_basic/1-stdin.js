f43b251957a4d654a4db341933e09b99029b2ed8
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 01:32:37 +0300

    Reads data passed through process.stdin and displays it to the standard output, replaced process.stdout.write with console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,5 @@ process.stdin.setEncoding('UTF8');
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) {
-    console.log(`Your name is: ${name.trim()}`);
-  }
+  process.stdout.write(`Your name is: ${name}`);
 });

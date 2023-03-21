8c243d86abe6f2be516dd31a74f318b1a2d35876
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 23:15:08 +0300

    Handled exit condition

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   if (input) {
-    console.log(`Your name is: ${input}`);
+    process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
+    process.exit(0);
   }
 });
 process.stdin.on('close', () => {

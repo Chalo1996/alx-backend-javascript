0fd902890c7e482bb7fab6de293e51c06a94c278
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 21:31:58 +0300

    added exit statement

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,6 @@
-process.stdin.setEncoding('utf8');
-
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (name) => {
+  process.stdout.write('Your name is: ' + name);
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });

c654c835e1cd4ebdff6d8cf3850f1a07144e6406
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:28:03 +0300

    added encoding and removed template string

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
+process.stdin.setEncoding('utf8');
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
+  process.stdout.write('Your name is: ' + input.trim() + '\n');
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });

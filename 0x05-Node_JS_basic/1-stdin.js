47e462ad76d6f28fe2a921e0b3cf492c5ab7c125
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 21:18:15 +0300

    added process.exit()

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
-process.stdin.setEncoding('UTF8');
+process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   process.stdout.write(`Your name is: ${name}`);
+  process.exit();
 });

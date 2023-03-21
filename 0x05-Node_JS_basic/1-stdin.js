7bbaa1267594c29e32e0e2357fa244901f33d2ac
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:39:45 +0300

    replaced the closing stdout message with console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,6 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
   process.exit();
 });

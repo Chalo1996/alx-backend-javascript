5ce53c5ad2362c4333415dad9f1168b7838907dd
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:41:35 +0300

    replaced all process.stdout.write with console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
+  console.log(`Your name is: ${input.toString().trim()}`);
   console.log('This important software is now closing');
   process.exit();
 });

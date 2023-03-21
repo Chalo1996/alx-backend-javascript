12eab8a7959f786e85443b4c5b7748275d1c77b5
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-22 01:13:09 +0300

    Reads dabase data asynchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = function countStudents (path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const linechuck = data.split('\n');
+
+    const lines = linechuck.filter((line) => line.trim() !== '');
+
+    const students = lines.length - 1;
+
+    console.log(`Number of students: ${students}`);
+
+    const fields = ['CS', 'SWE'];
+
+    for (const field of fields) {
+      const studentsInField = lines.filter((line) => line.includes(field));
+
+      console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.map((student) => student.split(',')[0]).join(', ')}`);
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};

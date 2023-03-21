593128cbecf6707fa27e54745ee7055bb3fada62
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:48:37 +0300

    Added a condition to discard null inputs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  console.log(`Your name is: ${input.toString().trim()}`);
-  console.log('This important software is now closing');
-  process.exit();
+  while ((input) !== null) {
+    console.log(`Your name is: ${input.toString().trim()}`);
+    console.log('This important software is now closing');
+    process.exit();
+  }
 });

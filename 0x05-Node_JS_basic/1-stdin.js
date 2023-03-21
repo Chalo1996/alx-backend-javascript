9370af059f029d7027b426ccd05ed6419937130c
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 23:07:44 +0300

    Removed toString.trim()

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input) {
-    console.log(`Your name is: ${input}`);
+    console.log(`Your name is: ${input.toString().trim()}`);
   }
 });
 process.stdin.on('close', () => {

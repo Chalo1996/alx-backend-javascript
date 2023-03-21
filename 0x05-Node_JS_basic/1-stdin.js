9172c7285426fbd28c071a8d30c92ed50801b164
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:37:33 +0300

    removed the encoding header and replaced the first stdout with console.log

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
-process.stdin.setEncoding('utf8');
-
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write('Your name is: ' + input.trim() + '\n');
+  process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });

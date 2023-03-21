41505d41c9a24f4aa393182b03d99c0ad6275841
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:22:53 +0300

    replaced data with readable to allow process to directly read data from the terminal

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (input) => {
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
   process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
   process.stdout.write('This important software is now closing\n');
   process.exit();

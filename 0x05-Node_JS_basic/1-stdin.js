d88a69ac2f3e518ce073e0b1ea46e50a5f6f93b3
Emmanuel Chalo <emusyoka759@gmail.com>
2023-03-21 22:59:44 +0300

    Defined a closing event to handle exit and replaced all console.log functions with process.stdout.write functions

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  while ((input) !== null) {
-    console.log(`Your name is: ${input.toString().trim()}`);
-    console.log('This important software is now closing');
-    process.exit();
+  if (input) {
+    process.stdout.write(`Your name is: ${input.toString().trim()}\n`);
   }
+});
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });

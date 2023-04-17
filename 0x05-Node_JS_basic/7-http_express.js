c7ca3ab61971a0a8059d5f1f7edc0ccf9dacffa7
Chalo1996 <emusyoka759@gmail.com>
2023-04-17 10:21:28 +0300

    Creates an express server that calls an asynchrous function

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+const port = 1245;
+const database = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  let msg = 'This is the list of our students\n';
+  try {
+    const output = await countStudents(database);
+    msg += output;
+    res.status(200).send(msg);
+  } catch (error) {
+    console.log(`This is the list of our students
+    Cannot load the database`);
+    res.status(500).end(`This is the list of our students
+    Cannot load the database`);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;

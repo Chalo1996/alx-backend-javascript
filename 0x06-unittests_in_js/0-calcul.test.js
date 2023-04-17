65ae1acc932338929e2d6d413526344a74779792
Chalo1996 <emusyoka759@gmail.com>
2023-04-17 23:56:50 +0300

    Test script

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
   it('should return the rounded sum of two positive numbers', function() {
-    assert.strictEqual(calculateNumber(2.4, 3.6), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it('should return the rounded sum of two negative numbers', function() {
-    assert.strictEqual(calculateNumber(-2.4, -3.6), -6);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('should return the rounded sum of a positive and a negative number', function() {
-    assert.strictEqual(calculateNumber(2.4, -3.6), -2);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('should return the rounded sum of a number and zero', function() {
-    assert.strictEqual(calculateNumber(2.4, 0), 2);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });

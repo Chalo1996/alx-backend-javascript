867cce3723d6ef2a4506d8dacd1ca57fd43f7110
Chalo1996 <emusyoka759@gmail.com>
2023-04-17 18:36:35 +0300

    Load app locals

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,7 +5,7 @@ import readDatabase from '../utils';
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
-      const data = await readDatabase('database.csv');
+      const data = await readDatabase(req.app.locals.database);
       res.status(200).send(`This is the list of our students\n${data}`);
     } catch (err) {
       res.status(500).send(err.message);
@@ -18,7 +18,7 @@ class StudentsController {
       res.status(500).send('Major parameter must be CS or SWE');
     } else {
       try {
-        const data = await readDatabase('database.csv');
+        const data = await readDatabase(req.app.locals.database);
         const lines = data.trim().split('\n');
         const regex = new RegExp(`^Number of students in ${major}:`);
         const filteredLines = lines.filter((line) => regex.test(line));

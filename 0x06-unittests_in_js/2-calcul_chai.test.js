4acecdf9ac31224e58a16d96d48ca921f11a74f9
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 11:45:13 +0300

    Using chai for testing

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,7 +1,31 @@
 const expect = require( 'chai' ).expect;
+const assert = require( 'chai' ).assert;
 const calculateNumber = require( "./1-calcul.js" );
 
 describe( 'calculateNumber', () => {
+    it( 'checks for sum', () => {
+        assert.equal( calculateNumber( 'SUM', 1, 3 ),
+            4 );
+        assert.equal( calculateNumber( 'SUM', 1, 3.7 ),
+            5 );
+        assert.equal( calculateNumber( 'SUM', 3.7, 1 ),
+            5 );
+        assert.equal( calculateNumber( 'SUM', 1.4, 4.5 ),
+            6 );
+    } );
+    it( 'checks for subtract', () => {
+        assert.equal( calculateNumber( 'SUBTRACT', 1, 3 ),
+            -2 );
+        assert.equal( calculateNumber( 'SUBTRACT', 1, 3.7 ), -3 );
+        assert.equal( calculateNumber( 'SUBTRACT', 3.7,
+            1 ), 3 );
+        assert.equal( calculateNumber( 'SUBTRACT', 1.4, 4.5 ), -4 );
+    } );
+    it( 'checks for divide', () => {
+        assert.equal( calculateNumber( 'DIVIDE', 1.4, 4.5 ), 0.2 );
+        assert.equal( calculateNumber( 'DIVIDE', 1.4, 0 ),
+            'Error' );
+    } );
     describe( 'When the type is SUM', () => {
         it( 'checks for sum', () => {
             expect( calculateNumber( 'SUM', 1, 3 ), 4 );

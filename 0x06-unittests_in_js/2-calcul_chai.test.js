51cfe69f02d57c7dc78e5664dfb9d0f63917dce2
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 11:55:25 +0300

    Using chai for testing

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,53 +1,27 @@
-const expect = require( 'chai' ).expect;
-const assert = require( 'chai' ).assert;
+const { expect } = require( "chai" );
 const calculateNumber = require( "./1-calcul.js" );
 
-describe( 'calculateNumber', () => {
+describe( 'calculateNumber When the type is SUM', () => {
     it( 'checks for sum', () => {
-        assert.equal( calculateNumber( 'SUM', 1, 3 ),
-            4 );
-        assert.equal( calculateNumber( 'SUM', 1, 3.7 ),
-            5 );
-        assert.equal( calculateNumber( 'SUM', 3.7, 1 ),
-            5 );
-        assert.equal( calculateNumber( 'SUM', 1.4, 4.5 ),
-            6 );
+        expect( calculateNumber( 'SUM', 1, 3 ), 4 );
+        expect( calculateNumber( 'SUM', 1, 3.7 ), 5 );
+        expect( calculateNumber( 'SUM', 3.7, 1 ), 5 );
+        expect( calculateNumber( 'SUM', 1.4, 4.5 ), 6 );
     } );
+} );
+
+describe( 'calculateNumber When the type is SUBTRACT', () => {
     it( 'checks for subtract', () => {
-        assert.equal( calculateNumber( 'SUBTRACT', 1, 3 ),
-            -2 );
-        assert.equal( calculateNumber( 'SUBTRACT', 1, 3.7 ), -3 );
-        assert.equal( calculateNumber( 'SUBTRACT', 3.7,
-            1 ), 3 );
-        assert.equal( calculateNumber( 'SUBTRACT', 1.4, 4.5 ), -4 );
+        expect( calculateNumber( 'SUBTRACT', 1, 3 ), -2 );
+        expect( calculateNumber( 'SUBTRACT', 1, 3.7 ), -3 );
+        expect( calculateNumber( 'SUBTRACT', 3.7, 1 ), 3 );
+        expect( calculateNumber( 'SUBTRACT', 1.4, 4.5 ), -4 );
     } );
+} );
+
+describe( 'calculateNumber When the type is DIVIDE', () => {
     it( 'checks for divide', () => {
-        assert.equal( calculateNumber( 'DIVIDE', 1.4, 4.5 ), 0.2 );
-        assert.equal( calculateNumber( 'DIVIDE', 1.4, 0 ),
-            'Error' );
-    } );
-    describe( 'When the type is SUM', () => {
-        it( 'checks for sum', () => {
-            expect( calculateNumber( 'SUM', 1, 3 ), 4 );
-            expect( calculateNumber( 'SUM', 1, 3.7 ), 5 );
-            expect( calculateNumber( 'SUM', 3.7, 1 ), 5 );
-            expect( calculateNumber( 'SUM', 1.4, 4.5 ), 6 );
-        } );
-    } );
-
-    describe( 'When the type is SUBTRACT', () => {
-        it( 'checks for subtract', () => {
-            expect( calculateNumber( 'SUBTRACT', 1, 3 ), -2 );
-            expect( calculateNumber( 'SUBTRACT', 1, 3.7 ), -3 );
-            expect( calculateNumber( 'SUBTRACT', 3.7, 1 ), 3 );
-            expect( calculateNumber( 'SUBTRACT', 1.4, 4.5 ), -4 );
-        } );
-    } );
-
-    describe( 'When the type is DIVIDE', () => {
-        it( 'checks for divide', () => {
-            expect( calculateNumber( 'DIVIDE', 1.4, 4.5 ), 0.2 );
-            expect( calculateNumber( 'DIVIDE', 1.4, 0 ), 'Error' );
-        } );
+        expect( calculateNumber( 'DIVIDE', 1.4, 4.5 ), 0.2 );
+        expect( calculateNumber( 'DIVIDE', 1.4, 0 ), 'Error' );
     } );
 } );

d48f7c150da7cb08dd936f2928a04f433ae0b3a6
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 10:35:45 +0300

    Reduced test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', () => {
-  it('rounds the first argument', () => {
+  it('it round the first argument', () => {
     assert.equal(calculateNumber(1.0, 1), 2);
     assert.equal(calculateNumber(1.3, 1), 2);
     assert.equal(calculateNumber(1.7, 1), 3);
   });
 
-  it('rounds the second argument', () => {
+  it('it round the second argument', () => {
     assert.equal(calculateNumber(1, 1.0), 2);
     assert.equal(calculateNumber(1, 1.3), 2);
     assert.equal(calculateNumber(1, 1.7), 3);
   });
 
-  it('shoulds return the right number', () => {
+  it('it should return the right number', () => {
     assert.equal(calculateNumber(1.2, 1), 2);
     assert.equal(calculateNumber(1, 1.2), 2);
     assert.equal(calculateNumber(1.2, 1.2), 2);

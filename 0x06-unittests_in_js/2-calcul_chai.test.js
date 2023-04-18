7c93629d222bf483dac1fab61851570a1bb23b45
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 12:06:35 +0300

    Bug Fixing

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,27 +1,40 @@
-const { expect } = require( "chai" );
-const calculateNumber = require( "./1-calcul.js" );
+'use strict';
+const chai = require( 'chai' );
+const calculateNumber = require( './2-calcul_chai.js' );
 
-describe( 'calculateNumber When the type is SUM', () => {
-    it( 'checks for sum', () => {
-        expect( calculateNumber( 'SUM', 1, 3 ), 4 );
-        expect( calculateNumber( 'SUM', 1, 3.7 ), 5 );
-        expect( calculateNumber( 'SUM', 3.7, 1 ), 5 );
-        expect( calculateNumber( 'SUM', 1.4, 4.5 ), 6 );
+describe( 'calculateNumber type is SUM', () => {
+    it( 'checks the output', () => {
+        chai.expect( calculateNumber( 'SUM', 1, 3.2 ) ).to.equal( 4 );
+        chai.expect( calculateNumber( 'SUM', 1.2, 3.7 ) ).to.equal( 5 );
+        chai.expect( calculateNumber( 'SUM', 3.7, 1.2 ) ).to.equal( 5 );
+        chai.expect( calculateNumber( 'SUM', 1.3, 4.5 ) ).to.equal( 6 );
+        chai.expect( calculateNumber( 'SUM', 4.6, 1.2 ) ).to.equal( 6 );
+        chai.expect( calculateNumber( 'SUM', 0.0, 0 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'SUM', -2, 2 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'SUM', 2, -2 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'SUM', -3, -3.2 ) ).to.equal( -6 );
     } );
 } );
-
-describe( 'calculateNumber When the type is SUBTRACT', () => {
-    it( 'checks for subtract', () => {
-        expect( calculateNumber( 'SUBTRACT', 1, 3 ), -2 );
-        expect( calculateNumber( 'SUBTRACT', 1, 3.7 ), -3 );
-        expect( calculateNumber( 'SUBTRACT', 3.7, 1 ), 3 );
-        expect( calculateNumber( 'SUBTRACT', 1.4, 4.5 ), -4 );
+describe( 'calculateNumber type is SUBTRACT', () => {
+    it( 'checks the output', () => {
+        chai.expect( calculateNumber( 'SUBTRACT', 5.2, 3 ) ).to.equal( 2 );
+        chai.expect( calculateNumber( 'SUBTRACT', 3.0, 2.5 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'SUBTRACT', 4.6, 2 ) ).to.equal( 3 );
+        chai.expect( calculateNumber( 'SUBTRACT', 0.0, 5 ) ).to.equal( -5 );
+        chai.expect( calculateNumber( 'SUBTRACT', 2, 4.8 ) ).to.equal( -3 );
+        chai.expect( calculateNumber( 'SUBTRACT', -1, -1 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'SUBTRACT', -1.9, 0 ) ).to.equal( -2 );
     } );
 } );
-
-describe( 'calculateNumber When the type is DIVIDE', () => {
-    it( 'checks for divide', () => {
-        expect( calculateNumber( 'DIVIDE', 1.4, 4.5 ), 0.2 );
-        expect( calculateNumber( 'DIVIDE', 1.4, 0 ), 'Error' );
+describe( 'calculateNumber type is DIVIDE', () => {
+    it( 'check the output', () => {
+        chai.expect( calculateNumber( 'DIVIDE', 16.2, 2 ) ).to.equal( 8 );
+        chai.expect( calculateNumber( 'DIVIDE', 2, 2 ) ).to.equal( 1 );
+        chai.expect( calculateNumber( 'DIVIDE', 8.5, 2 ) ).to.equal( 4.5 );
+        chai.expect( calculateNumber( 'DIVIDE', 0, 40 ) ).to.equal( 0 );
+        chai.expect( calculateNumber( 'DIVIDE', 1.44, 4.5 ) ).to.equal( 0.2 );
+        chai.expect( calculateNumber( 'DIVIDE', -1.4, 1 ) ).to.equal( -1 );
+        chai.expect( calculateNumber( 'DIVIDE', 1.006, 0 ) ).to.equal( 'Error' );
+        chai.expect( calculateNumber( 'DIVIDE', 100.5, 0.2 ) ).to.equal( 'Error' );
     } );
 } );

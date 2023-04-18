0ca53803c04d932da167e96153ebf159db153c15
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 10:33:46 +0300

    Reduced test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,37 +1,25 @@
-const assert = require( 'assert' );
-const mocha = require( 'mocha' );
-const calculateNumber = require( './0-calcul' );
+const assert = require("assert");
+const calculateNumber = require("./0-calcul.js");
 
-describe( 'calculateNumber', () => {
-  it( 'checks the output', () => {
-    assert.strictEqual( calculateNumber( 1, 3 ), 4 );
-    assert.strictEqual( calculateNumber( 1.4, 4.5 ), 6 );
-    assert.strictEqual( calculateNumber( 1.5, 3.7 ), 6 );
-    assert.strictEqual( calculateNumber( 1.2, 3.7 ), 5 );
-    assert.strictEqual( calculateNumber( 1.7, 0 ), 2 );
-  } );
-  it( 'checks for the first number', () => {
-    assert.strictEqual( calculateNumber( 3, 1 ), 4 );
-    assert.strictEqual( calculateNumber( 4.5, 1.4 ),
-      6 );
-    assert.strictEqual( calculateNumber( 3.7, 1.5 ),
-      6 );
-    assert.strictEqual( calculateNumber( 3.7, 1.2 ),
-      5 );
-  } );
-  it( 'checks for the second number', () => {
-    assert.strictEqual( calculateNumber( 1.3, 0 ), 1 );
-    assert.strictEqual( calculateNumber( 0, 1.2 ), 1 );
-    assert.strictEqual( calculateNumber( 1.3, 1.3 ), 2 );
-    assert.strictEqual( calculateNumber( 1.6, 1.8 ), 4 );
-    assert.strictEqual( calculateNumber( -1, -3.7 ), -5 );
-  } );
-  it('checks for NaN', () => {
-  try {
-    calculateNumber(NaN, 3.7);
-  } catch (error) {
-    assert.strictEqual(error.message, 'Invalid number');
-  }
+describe('calculateNumber', () => {
+  it('rounds the first argument', () => {
+    assert.equal(calculateNumber(1.0, 1), 2);
+    assert.equal(calculateNumber(1.3, 1), 2);
+    assert.equal(calculateNumber(1.7, 1), 3);
+  });
+
+  it('rounds the second argument', () => {
+    assert.equal(calculateNumber(1, 1.0), 2);
+    assert.equal(calculateNumber(1, 1.3), 2);
+    assert.equal(calculateNumber(1, 1.7), 3);
+  });
+
+  it('shoulds return the right number', () => {
+    assert.equal(calculateNumber(1.2, 1), 2);
+    assert.equal(calculateNumber(1, 1.2), 2);
+    assert.equal(calculateNumber(1.2, 1.2), 2);
+    assert.equal(calculateNumber(1.6, 1.1), 3);
+    assert.equal(calculateNumber(1.2, 1.9), 3);
+    assert.equal(calculateNumber(1.5, 1.9), 4);
+  });
 });
-
-} );

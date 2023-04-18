e591325c3e3f9d297ebbd8d808c19d9b98397ae4
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 10:29:52 +0300

    Reduced test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,17 +3,14 @@ const mocha = require( 'mocha' );
 const calculateNumber = require( './0-calcul' );
 
 describe( 'calculateNumber', () => {
-  it( 'it checks the output', () => {
+  it( 'checks the output', () => {
     assert.strictEqual( calculateNumber( 1, 3 ), 4 );
     assert.strictEqual( calculateNumber( 1.4, 4.5 ), 6 );
     assert.strictEqual( calculateNumber( 1.5, 3.7 ), 6 );
     assert.strictEqual( calculateNumber( 1.2, 3.7 ), 5 );
-    assert.strictEqual( calculateNumber( 0, 0 ), 0 );
-    assert.strictEqual( calculateNumber( 1.0, 0 ), 1 );
-    assert.strictEqual( calculateNumber( 1.3, 0 ), 1 );
     assert.strictEqual( calculateNumber( 1.7, 0 ), 2 );
   } );
-  it( 'it checks for the first number', () => {
+  it( 'checks for the first number', () => {
     assert.strictEqual( calculateNumber( 3, 1 ), 4 );
     assert.strictEqual( calculateNumber( 4.5, 1.4 ),
       6 );
@@ -21,33 +18,20 @@ describe( 'calculateNumber', () => {
       6 );
     assert.strictEqual( calculateNumber( 3.7, 1.2 ),
       5 );
-    assert.strictEqual( calculateNumber( 0, 0 ), 0 );
-    assert.strictEqual( calculateNumber( 0, 1.0 ), 1 );
-    assert.strictEqual( calculateNumber( 0, 1.3 ), 1 );
-    assert.strictEqual( calculateNumber( 0, 1.7 ), 2 );
   } );
-  it( 'it checks for the second number', () => {
+  it( 'checks for the second number', () => {
     assert.strictEqual( calculateNumber( 1.3, 0 ), 1 );
     assert.strictEqual( calculateNumber( 0, 1.2 ), 1 );
     assert.strictEqual( calculateNumber( 1.3, 1.3 ), 2 );
-    assert.strictEqual( calculateNumber( 1.7, 1.2 ), 3 );
-    assert.strictEqual( calculateNumber( 1.3, 1.8 ), 3 );
     assert.strictEqual( calculateNumber( 1.6, 1.8 ), 4 );
     assert.strictEqual( calculateNumber( -1, -3.7 ), -5 );
-    assert.strictEqual( calculateNumber( -1.2, -3.7 ), -5 );
-    assert.strictEqual( calculateNumber( -1.5, -3.7 ), -5 );
-    assert.strictEqual( calculateNumber( -1.2, -3.07 ), -4 );
-    assert.strictEqual( calculateNumber( -1, -3 ), -4 );
-    assert.strictEqual( calculateNumber( -1.2, 3.7 ), 3 );
-    assert.strictEqual( calculateNumber( 1, -1 ), 0 );
-    assert.strictEqual( calculateNumber( 1.5, -3.7 ), -2 );
-    assert.strictEqual( calculateNumber( 1.2, -3.7 ), -3 );
-    assert.strictEqual( calculateNumber( 3.5, -3.7 ), 0 );
-    assert.strictEqual( calculateNumber( 0, 3.7 ), 4 );
-    assert.strictEqual( calculateNumber( 0, 3.2 ), 3 );
-    assert.strictEqual( calculateNumber( 0, 3.5 ), 4 );
-    assert.strictEqual( calculateNumber( 0, 9.7 ), 10 );
-    assert.strictEqual( calculateNumber( 0, 9.2 ), 9 );
-    assert.strictEqual( calculateNumber( 0, 2.5 ), 3 );
   } );
+  it('checks for NaN', () => {
+  try {
+    calculateNumber(NaN, 3.7);
+  } catch (error) {
+    assert.strictEqual(error.message, 'Invalid number');
+  }
+});
+
 } );

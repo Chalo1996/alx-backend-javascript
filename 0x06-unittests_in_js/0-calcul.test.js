49598c30b1c8fa8049ca9b93626ff7279797044b
Chalo1996 <emusyoka759@gmail.com>
2023-04-18 10:15:52 +0300

    Reduced test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,9 @@
 const assert = require( 'assert' );
+const mocha = require( 'mocha' );
 const calculateNumber = require( './0-calcul' );
 
 describe( 'calculateNumber', () => {
-  it( 'it round the first argument', () => {
+  it( 'it checks the output', () => {
     assert.strictEqual( calculateNumber( 1, 3 ), 4 );
     assert.strictEqual( calculateNumber( 1.4, 4.5 ), 6 );
     assert.strictEqual( calculateNumber( 1.5, 3.7 ), 6 );
@@ -12,7 +13,7 @@ describe( 'calculateNumber', () => {
     assert.strictEqual( calculateNumber( 1.3, 0 ), 1 );
     assert.strictEqual( calculateNumber( 1.7, 0 ), 2 );
   } );
-  it( 'it round the second argument', () => {
+  it( 'it checks for the first number', () => {
     assert.strictEqual( calculateNumber( 3, 1 ), 4 );
     assert.strictEqual( calculateNumber( 4.5, 1.4 ),
       6 );
@@ -25,7 +26,7 @@ describe( 'calculateNumber', () => {
     assert.strictEqual( calculateNumber( 0, 1.3 ), 1 );
     assert.strictEqual( calculateNumber( 0, 1.7 ), 2 );
   } );
-  it( 'checks for correct output', () => {
+  it( 'it checks for the second number', () => {
     assert.strictEqual( calculateNumber( 1.3, 0 ), 1 );
     assert.strictEqual( calculateNumber( 0, 1.2 ), 1 );
     assert.strictEqual( calculateNumber( 1.3, 1.3 ), 2 );

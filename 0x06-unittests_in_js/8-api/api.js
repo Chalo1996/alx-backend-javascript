b9d3dfbb779b6e5a34dd8598b25179f63dcf2ace
Chalo1996 <emusyoka759@gmail.com>
2023-04-19 12:22:25 +0300

    The app

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 7865;
 
 app.get( '/', ( req, res ) => {
-    return res.status( 200 ).send( 'Welcome to the payment system');
+    res.status( 200 ).send( 'Welcome to the payment system' );
 })
 
 app.listen( PORT, () => {

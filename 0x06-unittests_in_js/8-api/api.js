9efcd8f239e625790fa36499ddbb1a059186afb4
Chalo1996 <emusyoka759@gmail.com>
2023-04-19 12:31:18 +0300

    Fix bug on correct output

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,11 +1,12 @@
+'use strict';
 const express = require( 'express' );
-
 const app = express();
+const PORT = 7865;
 
 app.get( '/', ( req, res ) => {
-    res.send( 'Welcome to the payment system' );
-} );
+    res.status( 200 ).send( 'Welcome to the payment system' );
+})
 
-app.listen( 7865, () => {
-    console.log( 'API available on localhost port 7865' );
-} );
+app.listen( PORT, () => {
+    console.log( `API available on localhost port ${ PORT }` );
+})

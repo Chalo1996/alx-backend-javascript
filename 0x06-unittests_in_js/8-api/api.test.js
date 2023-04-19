f736827b399b9e597d66213a0d29ffaf98504392
Chalo1996 <emusyoka759@gmail.com>
2023-04-19 12:14:49 +0300

    Api testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const request = require( 'request' );
+const expect = require( 'chai' ).expect;
+
+describe( 'Index page', () => {
+    it( 'should return status code 200', ( done ) => {
+        request.get( 'http://localhost:7865/', ( error, response ) => {
+            expect( response.statusCode ).to.equal( 200 );
+            done();
+        } );
+    } );
+
+    it( 'should return the message "Welcome to the payment system"', ( done ) => {
+        request.get( 'http://localhost:7865/', ( error, response, body ) => {
+            expect( body ).to.equal( 'Welcome to the payment system' );
+            done();
+        } );
+    } );
+} );

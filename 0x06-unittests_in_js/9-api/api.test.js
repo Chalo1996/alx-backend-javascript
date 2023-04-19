b3648e93c817cafb399a0c5e98273062611ca0a3
Chalo1996 <emusyoka759@gmail.com>
2023-04-19 12:56:51 +0300

    Test for correct params

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require( 'request' );
+const expect = require( 'chai' ).expect;
+
+describe( 'Index page', () => {
+    it( 'should return status code 200', ( done ) => {
+        request.get( 'http://localhost:7865/', ( error, response ) => {
+            expect( response.statusCode ).to.equal( 200 );
+            done();
+        } );
+    } );
+
+    it( 'should return the message "Welcome to the payment system"', ( done ) => {
+        request.get( 'http://localhost:7865/', ( error, response, body ) => {
+            expect( body ).to.equal( 'Welcome to the payment system' );
+            done();
+        } );
+    } );
+
+    it( 'should return status code 200 when :id is a number', ( done ) => {
+        request.get( 'http://localhost:7865/cart/123', ( error, response ) => {
+            expect( response.statusCode ).to.equal( 200 );
+            done();
+        } );
+    } );
+
+    it( 'should return status code 404 when :id is NOT a number', ( done ) => {
+        request.get( 'http://localhost:7865/cart/abc', ( error, response ) => {
+            expect( response.statusCode ).to.equal( 404 );
+            done();
+        } );
+    } );
+} );

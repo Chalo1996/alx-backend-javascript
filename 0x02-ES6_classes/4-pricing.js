94d385e79bb90fd1b984fb1217b129a7491cbf9e
Chalo1996 <emusyoka759@gmail.com>
2023-04-22 00:42:16 +0300

    Bug Fixing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -7,42 +7,20 @@ export default class Pricing {
     }
 
     if (!(currency instanceof Currency)) {
-      throw new TypeError('currency must be of type Currency');
+      throw new TypeError('Currency must be an instance of Currency');
     }
 
     this._amount = amount;
-    this._currency = currency;
-  }
-
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = amount;
-  }
-
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('currency must be of type Currency');
-    }
     this._currency = currency;
   }
 
   displayFullPrice() {
-    return (`${this._amount} ${this._currency.name} (${this._currency.code}))`);
+    return (`${this._amount} ${this._currency.displayFullCurrency()}`);
   }
 
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Both amount and conversionRate must be numbers');
+      throw new TypeError('Amount and conversion rate must be numbers');
     }
     return amount * conversionRate;
   }

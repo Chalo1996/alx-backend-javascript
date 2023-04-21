5ca9534a735b9e8cd5fce24a67c5c2a22bbdbc09
Chalo1996 <emusyoka759@gmail.com>
2023-04-22 00:45:43 +0300

    Add seters and getters Fixing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -14,6 +14,24 @@ export default class Pricing {
     this._currency = currency;
   }
 
+  get amount() { return this._amount; }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() { return this._currency; }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
   displayFullPrice() {
     return (`${this._amount} ${this._currency.displayFullCurrency()}`);
   }

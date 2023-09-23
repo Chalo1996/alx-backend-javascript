aaef3623bbdcbcbecf81aba4ccd0c2af567dd49e
Chalo1996 <emusyoka759@gmail.com>
2023-09-23 23:03:30 +0300

    Fix lint error.

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,9 @@
-export default function createReportObject ( employeesList ) {
+export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments ( employeesList ) {
-      let depsCount = 0;
-      for ( let _deps of Object.keys( employeesList ) ) {
-        depsCount += 1;
-      }
+    getNumberOfDepartments(employeesList) {
+      const depsCount = Object.keys(employeesList).length;
       return depsCount;
-    }
-  }
-};
+    },
+  };
+}

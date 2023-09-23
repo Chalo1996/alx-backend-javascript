b5f20adee3423888719e0dcc28dd4b8cff7d1df5
Chalo1996 <emusyoka759@gmail.com>
2023-09-23 21:54:41 +0300

    Fix Bug: Fix variable declaration

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject ( employeesList ) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments ( employeesList ) {
-      depsCount = 0;
+      let depsCount = 0;
       for ( let _deps of Object.keys( employeesList ) ) {
         depsCount += 1;
       }
